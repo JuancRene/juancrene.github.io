394669b1c15a746e202698273c3dc6e14e0a626d
Juan Cruz Rene <102224698+JuancRene@users.noreply.github.com>
2025-03-24 13:47:32 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Juan Cruz Rene Arenas | Desarrollador de Software",
+  title: "Juan Rene | Desarrollador de Software",
   description:
     "Portfolio profesional de Juan Cruz Rene Arenas, desarrollador de software especializado en C#, HTML, CSS, Java y SQL.",
 }
