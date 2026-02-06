a4be45896bb8adce7de5919185263b5bf8e86a94
Juan Cruz Rene <102224698+JuancRene@users.noreply.github.com>
2026-02-06 19:31:45 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,15 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is no longer used. The application has been migrated to Vanilla JS in index.html.
+// To run the React version again, revert changes to index.html and this file.
+console.log("App running in Vanilla JS mode.");
